29184c236634f678eff01ef381cda95752b2f669
will <willie@rezserve.com>
2019-06-03 22:33:19 -0700

    Updates

---
+++ precache-manifest.ae1ee43167acb9782de8c565ad7cf98b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c0bb3bf2c26f4cf48340a2fbd2e7b1",
+    "url": "/comictree/index.html"
+  },
+  {
+    "revision": "e95bb7f5c3565aa13013",
+    "url": "/comictree/static/css/main.c7181fde.chunk.css"
+  },
+  {
+    "revision": "9c962d5964159b99eebf",
+    "url": "/comictree/static/js/2.568a6565.chunk.js"
+  },
+  {
+    "revision": "e95bb7f5c3565aa13013",
+    "url": "/comictree/static/js/main.db35c02f.chunk.js"
+  },
+  {
+    "revision": "43bf44f383c1ed156de4",
+    "url": "/comictree/static/js/runtime~main.377105fe.js"
+  }
+]);
